f99aa6a9950f77f88574ae013a445c8294b58af5
Alexander Mennborg <alexander.mennborg@gmail.com>
2020-09-25 09:25:53 +0200

    negative sampling and bigger dataset

--- src/word2vec.py
+++ src/word2vec.py
@@ -3,13 +3,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
+import random
 import data
 
-
-
-# Word2Vec - SkipGram version
+# Word2Vec - Skip-gram version
 #  
 #   Input (1-hot)    Hidden (embedding)    Output (softmax classifier)
 #     0                                       0.0
@@ -17,6 +15,15 @@ import data
 #     1        =>      0.1              =>    0.9
 #     0                0.9                    0.4
 #     0                                       0.1
+#
+# With negative sampling
+#  
+#   Input (1-hot)    Hidden (embedding)    Binary Classifier 0 or 1 
+#     0                                       
+#     0                0.3                    
+#     1        =>      0.1              =>    0.9 (1 neighbour, 0 not related)
+#     0                0.9                    
+#     0                                       
 #
 # In the SkipGram version we train neural network to classify which words
 # are related to the input word which in turn trains or word embedding.
@@ -31,29 +38,44 @@ class Word2Vec(nn.Module):
         x = self.embedding(x)
         return self.output(x)
 
+class Word2VecNegSampling(nn.Module):
+    def __init__(self, vocab_size, embedding_size):
+        super(Word2VecNegSampling, self).__init__()
+        self.embedding = nn.Linear(vocab_size, embedding_size, bias=False)
+        self.output = nn.Linear(embedding_size, 1, bias=False)
 
-def cuda_device_if_available():
-    """Returns the CUDA device if it is available, 
-    if no then the CPU device is returned instead.
-    """
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    device_name = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU"
-    print("Training on: " + device_name)
-    return device
+    def forward(self, x):
+        x = self.embedding(x)
+        return self.output(x)
 
+# Default hyper-parameters
+embedding_size = 30
+window_size = 2
+batch_size = 64
+num_epochs = 5000
+learning_rate=0.001
 
-def main():
-    # Hyper-parameters
-    embedding_size = 30
-    window_size = 2
-    batch_size = 64
-    num_epochs = 5
-    learning_rate=0.001
+# Dataset storage
+word_sequence = None
+vocab = None
+vocab_size = None
+skip_grams = None
+num_skip_grams = None
 
+# Training model
+model = None
+optimizer = None
+criterion = None
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device_name = torch.cuda.get_device_name(0) if torch.cuda.is_available() else "CPU"
+
+def setup_dataset(filename):
+    global word_sequence, vocab, vocab_size, skip_grams, num_skip_grams
     # Setup sequence of words and the vocabulary
-    word_sequence = data.word_sequence_from_korp_dataset("../dataset/flashback_emails.csv")
+    word_sequence = data.word_sequence_from_korp_dataset("../dataset/" + filename)
     vocab = list(set(word_sequence))
     vocab_size = len(vocab)
+    print("vocab_size:", vocab_size)
 
     # Make skip-grams by combining center word and surrounding context words
     skip_grams = list()
@@ -63,38 +85,23 @@ def main():
             if j != i and j >= 0 and j < len(word_sequence):
                 context_word = vocab.index(word_sequence[j])
                 skip_grams.append((center_word, context_word))
+    num_skip_grams = len(skip_grams)
 
     print("Some skip-gram examples:", [(vocab[i], vocab[j]) for (i, j) in skip_grams[:10]])
 
     # NOTE(alexander): embedding size should be smaller to get meaningful representation
     assert(embedding_size < vocab_size)
 
-
-    # If CUDA support use that
-    device = cuda_device_if_available()
-
-    # Create the word2vec model, optimizer and criterion
-    model = Word2Vec(vocab_size, embedding_size)
-    model = model.to(device)
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    criterion = nn.CrossEntropyLoss()
-
+def train_model(name, data_loader):
+    print("Training on: " + device_name)
     lowest_loss = 1000
-    filename = "checkpoint.pt"
+    filename = name + "_checkpoint.pt"
 
     # Train the model
-    for epoch in range(5000):
+    for epoch in range(num_epochs):
         # Create a mini batch
-        input_batch = list()
-        target_batch = list()
-        indices_batch = np.random.choice(range(len(skip_grams)), batch_size, replace=False)
-        for i in indices_batch:
-            input_batch.append(np.eye(vocab_size)[skip_grams[i][0]]) # center word
-            target_batch.append(skip_grams[i][1]) # context word
-
-        inputs = torch.Tensor(input_batch)
+        inputs, targets = data_loader()
         inputs = inputs.to(device)
-        targets = torch.LongTensor(target_batch)
         targets = targets.to(device)
 
         optimizer.zero_grad()
@@ -106,12 +113,119 @@ def main():
 
         if (epoch + 1) % 100 == 0:
             print("Epoch: %d - loss: %.6f" % (epoch + 1, epoch_loss))
-
-        if epoch_loss < lowest_loss:
-            torch.save(model.embedding.state_dict(), filename)
-            lowest_loss = epoch_loss
-            print("Reached lower loss of %.6f at epoch %d saving to %s" % (lowest_loss, epoch + 1, filename))
+            if epoch_loss < lowest_loss:
+                torch.save(model.embedding.state_dict(), filename)
+                lowest_loss = epoch_loss
+                print("Reached lower loss of %.6f at epoch %d saving to %s" % (lowest_loss, epoch + 1, filename))
     print("Finished Training")
 
+def load_samples():
+    input_batch = list()
+    target_batch = list()
+    indices_batch = np.random.choice(range(num_skip_grams), batch_size, replace=False)
+    for i in indices_batch:
+        input_batch.append(np.eye(vocab_size)[skip_grams[i][0]]) # center word
+        target_batch.append(skip_grams[i][1]) # context word
+
+    inputs = torch.Tensor(input_batch)
+    targets = torch.LongTensor(target_batch)
+    return inputs, targets
+
+def load_negative_samples():
+    input_batch = list()
+    target_batch = list()
+
+    center_word = random.randint(0, vocab_size - 1)
+    positive_samples = []
+    negative_samples = []
+    for sg in skip_grams:
+        if sg[0] == center_word: 
+            continue
+        if sg[1] == center_word:
+            positive_samples.append(sg[1])
+        else:
+            negative_samples.append(sg[1])
+
+    positive_sample = random.randint(0, len(positive_samples) - 1)
+    input_batch.append(np.eye(vocab_size)[skip_grams[positive_sample][0]]) # target skipgram
+    target_batch.append([0])
+
+    negative_samples = np.random.choice(negative_samples, batch_size, replace=False)
+    for i in negative_samples:
+        input_batch.append(np.eye(vocab_size)[i]) # center word
+        target_batch.append([1])
+
+    # print("Center word:", vocab[center_word])
+    # print("Positive sample:", vocab[positive_sample])
+    # print("Negative samples:")
+    # for sample in negative_samples:
+        # print("-", vocab[sample]);
+
+    inputs = torch.Tensor(input_batch)
+    targets = torch.Tensor(target_batch)
+    return inputs, targets
+
+def test_flashback_word2vec():
+    global model, optimizer, criterion
+
+    # Setup dataset
+    setup_dataset("flashback_emails.csv");
+
+    # Create the word2vec model, optimizer and criterion
+    model = Word2Vec(vocab_size, embedding_size)
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    train_model("flashback_word2vec", load_samples);
+
+def test_flashback_word2vec_neg_sampling():
+    global model, optimizer, criterion, batch_size
+
+    # Setup dataset
+    setup_dataset("flashback_emails.csv");
+
+    # Use smaller batch size for negative sampling
+    batch_size = 20
+
+    # Create the word2vec model, optimizer and criterion
+    model = Word2VecNegSampling(vocab_size, embedding_size)
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.BCEWithLogitsLoss()
+    train_model("flashback_word2vec", load_negative_samples);
+
+def test_literatur_word2vec():
+    global model, optimizer, criterion
+
+    # Setup dataset
+    setup_dataset("literaturbanken.csv");
+
+    # Create the word2vec model, optimizer and criterion
+    model = Word2Vec(vocab_size, embedding_size)
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+    train_model("literatur_word2vec", load_samples);
+
+def test_literatur_word2vec_neg_sampling():
+    global model, optimizer, criterion, batch_size
+
+    # Setup dataset
+    setup_dataset("literaturbanken.csv");
+
+    # Use smaller batch size for negative sampling
+    batch_size = 5
+
+    # Create the word2vec model, optimizer and criterion
+    model = Word2VecNegSampling(vocab_size, embedding_size)
+    model = model.to(device)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.BCEWithLogitsLoss()
+    train_model("literatur_word2vec", load_negative_samples);
+
+
 if __name__== "__main__":
-    main()
+    # test_flashback_word2vec()
+    # test_flashback_word2vec_neg_sampling()
+    # test_literatur_word2vec()
+    test_literatur_word2vec_neg_sampling()
--- src/word2vec_test.py
+++ src/word2vec_test.py
@@ -6,14 +6,27 @@ from data import word_sequence_from_korp_dataset
 import matplotlib.pyplot as plt
 
 
+def input_word(vocab):
+    while True:
+        word = input()
+        try:
+            word_idx = vocab.index(word)
+            return word_idx
+        except:
+            print("Word `" + word + "` is not in the vocabulary")
+    return 0
+                
+
 def main():
     # Load in the vocab
-    word_sequence = word_sequence_from_korp_dataset("../dataset/flashback_emails.csv")
+    # word_sequence = word_sequence_from_korp_dataset("../dataset/flashback_emails.csv")
+    word_sequence = word_sequence_from_korp_dataset("../dataset/literaturbanken.csv")
     vocab = list(set(word_sequence))
     vocab_size = len(vocab)
 
     # Load in the model
-    filename = "checkpoint.pt"
+    # filename = "flashback_word2vec_checkpoint.pt"
+    filename = "literatur_word2vec_checkpoint.pt"
     embedding_size = 30
     model = nn.Linear(vocab_size, embedding_size, bias=False)
     model.load_state_dict(torch.load(filename, map_location=torch.device('cpu')))
@@ -23,20 +36,43 @@ def main():
     for i, _ in enumerate(vocab):
         inputs = torch.Tensor(np.eye(vocab_size)[i])
         embedding = model(inputs)
-        input_embeddings[i] = embedding.detach().tolist()
+        input_embeddings[i] = embedding.detach().numpy()
 
-    tsne_embeddings = TSNE(n_components=2).fit_transform(np.array(input_embeddings))
-    x = [0]*vocab_size
-    y = [0]*vocab_size
-    for i, embd in enumerate(tsne_embeddings):
-        x[i] = embd[0]
-        y[i] = embd[1]
-        
-    fig, ax = plt.subplots()
-    ax.scatter(x, y)
-    for i, word in enumerate(vocab):
-        ax.annotate(word, (x[i] + 0.1, y[i] + 0.1))
-    plt.show()
+    # Plot t-SNE embeddings
+    if False:
+        tsne_embeddings = TSNE(n_components=2).fit_transform(np.array(input_embeddings))
+        x = [0]*vocab_size
+        y = [0]*vocab_size
+        for i, embd in enumerate(tsne_embeddings):
+            x[i] = embd[0]
+            y[i] = embd[1]
+
+        fig, ax = plt.subplots()
+        ax.scatter(x, y)
+        for i, word in enumerate(vocab):
+            ax.annotate(word, (x[i] + 0.1, y[i] + 0.1))
+        plt.show()
+
+    # Test word-embeddings
+    if True:
+        while True:
+            print("Vector algebra: Word1 - Word2 = Word3")
+            print("Word1:")
+            word1 = input_embeddings[input_word(vocab)];
+
+            print("Word2:")
+            word2 = input_embeddings[input_word(vocab)];
+
+            vec3 = word1-word2
+            shortest_dist = 1000
+            word3 = "nothing found"
+            for i, embed in enumerate(input_embeddings):
+                dist = np.linalg.norm(embed-vec3)
+                if dist < shortest_dist:
+                    shortest_dist = dist
+                    word3 = vocab[i]
+
+            print("Word3:", word3, " (shortest distance:", shortest_dist, ")")
 
 if __name__ == "__main__":
     main()
